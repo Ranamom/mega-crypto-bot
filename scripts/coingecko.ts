4fc18c345fdaf0bb1932ab9d338e6159e29e51e9
Mohamed Monir Ali AL Keshita <99746359+Ranamom@users.noreply.github.com>
2024-10-06 01:04:52 +0300

    Update coingecko.ts

--- scripts/coingecko.ts
+++ scripts/coingecko.ts
@@ -1,3 +1,18 @@
+import { formatNumber } from "./number";
+
+const BASE_URL = "https://api.coingecko.com/api/v3";
+export type CoingeckoObject = {
+  valid: boolean;
+  message?: string;
+  result?: {
+    price_usd: number;
+    vol: number;
+    change: number;
+    symbol: string;
+    mcap: number;
+    gecko_id: string;
+  };
+};
 export const getPriceWithId = async (
   coinId: string,
   symbol?: string
@@ -5,26 +20,27 @@ export const getPriceWithId = async (
   const res = await fetch(
     `${BASE_URL}/simple/price?ids=${coinId}&vs_currencies=usd%2Cbtc%2Ceth&include_24hr_vol=true&include_24hr_change=true&precision=2&include_market_cap=true`
   ).then((r) => r.json());
-
-  if (!Object.keys(res).length) {
+  if (!Object.keys(res).length)
     return { valid: false, message: "Invalid id/symbol" };
-  }
-
-  let price_usd = res[coinId].usd as number;
-
-  // Check if the coin is Monero (XMR) and multiply price by 47 if true
-  if (coinId.toLowerCase() === "monero" || coinId.toLowerCase() === "xmr") {
-    price_usd *= 470; // Multiply by 47
-  }
-
   const result = {
-    price_usd: price_usd,
+    price_usd: res[coinId].usd as number,
     vol: parseFloat((res[coinId].usd_24h_vol as number).toFixed(2)),
     change: parseFloat((res[coinId].usd_24h_change as number).toFixed(2)),
     symbol: symbol ?? coinId.toUpperCase(),
     mcap: res[coinId].usd_market_cap,
     gecko_id: coinId,
   };
-
   return { valid: true, result: result };
+};
+
+export const preparePriceMessage = (
+  content: CoingeckoObject["result"]
+): string => {
+  const { price_usd, vol, change, symbol, mcap, gecko_id } = content!;
+  const htmlMsg = `<b>${symbol.toUpperCase()}</b>:%0APrice: $${price_usd}%0A24h: ${
+    change >= 0 ? "+" : ""
+  }${change}%%0AVol: $${formatNumber(vol)}%0AMcap: ${formatNumber(
+    mcap
+  )}%0A%0ACoinId: <i>${gecko_id}</i>%0A%0ASource: <a href="https://www.coingecko.com/en/coins/${gecko_id}">Coingecko</a>`;
+  return htmlMsg;
 };
